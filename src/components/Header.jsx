afac7beb507f71c3de523b639308c1e18ebc85e3
ChocoCodes <chocojhong999@gmail.com>
2025-01-01 20:02:48 +0800

    Fixed: styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,13 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
-        <>
+        <header>
             <h1>Which element are you?</h1>
             <p>(based on completely random things)</p>
-            <Link to="/">Home</Link>  
-            <Link to="/quiz">Quiz</Link>  
-        </>
+            <nav>
+                <Link to="/">Home</Link>  
+                <Link to="/quiz">Quiz</Link>  
+            </nav>
+        </header>
     );
 }
